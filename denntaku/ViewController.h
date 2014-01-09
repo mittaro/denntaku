9bfec8e1ba9d75fd6019c32c4c11ad1aff262fa2
mittaro <2011531018@ma245.is.env.kitakyu-u.ac.jp>
2014-01-09 15:58:03 +0900

    denntaku

--- denntaku/ViewController.h
+++ denntaku/ViewController.h
@@ -8,11 +8,54 @@
 
 #import <UIKit/UIKit.h>
 
+int Method;
+int SelectNumber;
+int calcFlug;
+float RunningTotal;
+
+
 @interface ViewController : UIViewController
 
-- (IBAction)b1:(id)sender;
+{IBOutlet UILabel *Screen;
+}
+
+- (IBAction)OneButton:(id)sender;
+
+- (IBAction)TwoButton:(id)sender;
+
+- (IBAction)ThreeButton:(id)sender;
+
+- (IBAction)FourButton:(id)sender;
+
+- (IBAction)FiveButton:(id)sender;
+
+- (IBAction)SixButton:(id)sender;
+
+- (IBAction)SevenButton:(id)sender;
+
+- (IBAction)EightButton:(id)sender;
+
+- (IBAction)NineButton:(id)sender;
 
 
-@property (weak, nonatomic) IBOutlet UILabel *result;
+- (IBAction)ZeroButton:(id)sender;
+
+- (IBAction)AddButton:(id)sender;
+
+- (IBAction)SubtractButton:(id)sender;
+
+- (IBAction)MultiplyButton:(id)sender;
+
+- (IBAction)DevideButton:(id)sender;
+
+- (IBAction)EqualButton:(id)sender;
+
+- (IBAction)PulsAndMinusButton:(id)sender;
+
+- (IBAction)PointButton:(id)sender;
+
+- (IBAction)ClearButton:(id)sender;
+
+- (IBAction)DeleteButton:(id)sender;
 
 @end
--- denntaku/ViewController.m
+++ denntaku/ViewController.m
@@ -14,6 +14,281 @@
 
 @implementation ViewController
 
+
+- (IBAction)OneButton:(id)sender {
+    
+    SelectNumber = SelectNumber * 10;
+    SelectNumber = SelectNumber + 1;
+    Screen.text = [NSString stringWithFormat:@"%i",SelectNumber];
+    
+}
+
+- (IBAction)TwoButton:(id)sender {
+    
+    SelectNumber = SelectNumber * 10;
+    SelectNumber = SelectNumber + 2;
+    Screen.text = [NSString stringWithFormat:@"%i",SelectNumber];
+}
+
+- (IBAction)ThreeButton:(id)sender {
+    
+    SelectNumber = SelectNumber * 10;
+    SelectNumber = SelectNumber + 3;
+    Screen.text = [NSString stringWithFormat:@"%i",SelectNumber];
+    
+}
+
+- (IBAction)FourButton:(id)sender {
+    
+    SelectNumber = SelectNumber * 10;
+    SelectNumber = SelectNumber + 4;
+    Screen.text = [NSString stringWithFormat:@"%i",SelectNumber];
+    
+}
+
+- (IBAction)FiveButton:(id)sender {
+    
+    SelectNumber = SelectNumber * 10;
+    SelectNumber = SelectNumber + 5;
+    Screen.text = [NSString stringWithFormat:@"%i",SelectNumber];
+    
+}
+
+- (IBAction)SixButton:(id)sender {
+    
+    SelectNumber = SelectNumber * 10;
+    SelectNumber = SelectNumber + 6;
+    Screen.text = [NSString stringWithFormat:@"%i",SelectNumber];
+    
+}
+
+- (IBAction)SevenButton:(id)sender {
+    
+    SelectNumber = SelectNumber * 10;
+    SelectNumber = SelectNumber + 7;
+    Screen.text = [NSString stringWithFormat:@"%i",SelectNumber];
+    
+}
+
+- (IBAction)EightButton:(id)sender {
+    
+    SelectNumber = SelectNumber * 10;
+    SelectNumber = SelectNumber + 8;
+    Screen.text = [NSString stringWithFormat:@"%i",SelectNumber];
+    
+}
+
+- (IBAction)NineButton:(id)sender {
+    
+    SelectNumber = SelectNumber * 10;
+    SelectNumber = SelectNumber + 9;
+    Screen.text = [NSString stringWithFormat:@"%i",SelectNumber];
+    
+}
+
+- (IBAction)ZeroButton:(id)sender {
+    
+    SelectNumber = SelectNumber * 10;
+    SelectNumber = SelectNumber + 0;
+    Screen.text = [NSString stringWithFormat:@"%i",SelectNumber];
+    
+}
+
+- (IBAction)AddButton:(id)sender {
+    if (RunningTotal == 0) {
+        
+        RunningTotal = SelectNumber;
+    }
+    else{
+        switch (Method) {
+            case 1:
+                RunningTotal = RunningTotal * SelectNumber;
+                break;
+            case 2:
+                RunningTotal = RunningTotal / SelectNumber;
+                break;
+            case 3:
+                RunningTotal = RunningTotal - SelectNumber;
+                break;
+            case 4:
+                RunningTotal = RunningTotal + SelectNumber;
+                break;
+            default:
+                break;
+        }
+    }
+    calcFlug = 1;
+    Method =4;
+    SelectNumber = 0;
+}
+
+- (IBAction)SubtractButton:(id)sender {
+    if (RunningTotal == 0) {
+        
+        RunningTotal = SelectNumber;
+    }
+    else{
+        switch (Method) {
+            case 1:
+                RunningTotal = RunningTotal * SelectNumber;
+                break;
+            case 2:
+                RunningTotal = RunningTotal / SelectNumber;
+                break;
+            case 3:
+                RunningTotal = RunningTotal - SelectNumber;
+                break;
+            case 4:
+                RunningTotal = RunningTotal + SelectNumber;
+                break;
+            default:
+                break;
+        }
+    }
+    calcFlug = 2;
+      Method =3;
+    SelectNumber = 0;
+}
+
+- (IBAction)MultiplyButton:(id)sender {
+    if (RunningTotal == 0) {
+        
+        RunningTotal = SelectNumber;
+    }
+    else{
+        switch (Method) {
+            case 1:
+                RunningTotal = RunningTotal * SelectNumber;
+                break;
+            case 2:
+                RunningTotal = RunningTotal / SelectNumber;
+                break;
+            case 3:
+                RunningTotal = RunningTotal - SelectNumber;
+                break;
+            case 4:
+                RunningTotal = RunningTotal + SelectNumber;
+                break;
+            default:
+                break;
+        }
+    }
+    
+    calcFlug = 3;
+    Method =1;
+    SelectNumber = 0;
+}
+
+- (IBAction)DevideButton:(id)sender {
+    if (RunningTotal == 0) {
+        
+        RunningTotal = SelectNumber;
+    }
+    else{
+        switch (Method) {
+            case 1:
+                RunningTotal = RunningTotal * SelectNumber;
+                break;
+            case 2:
+                RunningTotal = RunningTotal / SelectNumber;
+                break;
+            case 3:
+                RunningTotal = RunningTotal - SelectNumber;
+                break;
+            case 4:
+                RunningTotal = RunningTotal + SelectNumber;
+                break;
+            default:
+                break;
+        }
+    }
+   calcFlug = 4;
+    Method =2;
+    SelectNumber = 0;
+    
+}
+
+- (IBAction)EqualButton:(id)sender {
+    if (RunningTotal == 0) {
+        
+        RunningTotal = SelectNumber;
+    }
+    else{
+        switch (Method) {
+            case 1:
+                RunningTotal = RunningTotal * SelectNumber;
+                break;
+            case 2:
+                RunningTotal = RunningTotal / SelectNumber;
+                break;
+            case 3:
+                RunningTotal = RunningTotal - SelectNumber;
+                break;
+            case 4:
+                RunningTotal = RunningTotal + SelectNumber;
+                break;
+            default:
+                break;
+        }
+    }
+    Method =0;
+    SelectNumber = 0;
+    Screen.text = [NSString stringWithFormat:@"%.3f",RunningTotal];
+}
+- (void)calc{
+    
+switch (calcFlug) {
+            
+            case 1: //足し算
+               RunningTotal += SelectNumber;
+               break;
+        
+            case 2: //引き算
+                RunningTotal -= SelectNumber;
+                break;
+            
+            case 3: //かけ算
+                RunningTotal *= SelectNumber;
+                break;
+            
+                case 4: //割り算
+                    RunningTotal /= SelectNumber;
+                break;
+            
+            default:
+                    break;
+            
+    }
+    
+    SelectNumber = RunningTotal;
+    Screen.text = [NSString stringWithFormat:@"0"];
+    SelectNumber = 0;
+    
+}
+
+
+
+- (IBAction)PulsAndMinusButton:(id)sender {
+}
+
+- (IBAction)PointButton:(id)sender {
+}
+
+- (IBAction)ClearButton:(id)sender {
+    Method = 0;
+    RunningTotal = 0;
+    SelectNumber = 0;
+    calcFlug = 0;
+    Screen.text = [NSString stringWithFormat:@"0"];
+    
+}
+
+- (IBAction)DeleteButton:(id)sender {
+}
+
+
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -27,9 +302,4 @@
 }
 
 
-- (IBAction)b1:(id)sender {
-    NSInteger num1 = 1;
-    　　NSString *print = [[NSString alloc] initWithFormat:@"%d!", num1];
-       self.numberOutput.text = print;
-}
 @end
